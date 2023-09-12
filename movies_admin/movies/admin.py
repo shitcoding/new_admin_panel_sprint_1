a6f5e687c73c63f8cf7255f3640291a4a862b8c0
shitcoding <shitcoder@pm.me>
2023-09-12 18:40:58 +0300

    add filmwork and genre models to admin panel

--- movies_admin/movies/admin.py
+++ movies_admin/movies/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Filmwork, Genre, GenreFilmwork
+
+
+class GenreFilmworkInline(admin.TabularInline):
+    model = GenreFilmwork
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Filmwork)
+class FilmworkAdmin(admin.ModelAdmin):
+    inlines = (GenreFilmworkInline,)
