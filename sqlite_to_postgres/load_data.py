a1db7447ed2735238351593d8d14d4124fa42fbb
shitcoding <shitcoder@pm.me>
2023-09-14 21:58:26 +0300

    add draft main load_data script

--- sqlite_to_postgres/load_data.py
+++ sqlite_to_postgres/load_data.py
@@ -1,20 +1,37 @@
 import sqlite3
 
 import psycopg2
+from connectors import sqlite_conn_context
+from db_dataclasses import (Filmwork, Genre, GenreFilmwork, Person,
+                            PersonFilmwork)
+from db_settings import DSL, SQLITE_DB_PATH
 from psycopg2.extensions import connection as _connection
 from psycopg2.extras import DictCursor
+from sqlite_extractor import SQLiteExtractor
+
+TABLES = (Genre, Filmwork, Person, PersonFilmwork, GenreFilmwork)
 
 
-def load_from_sqlite(connection: sqlite3.Connection, pg_conn: _connection):
+
+def load_from_sqlite(
+    sqlite_conn: sqlite3.Connection,
+    pg_conn: _connection,
+    chunk_size: int = 100,
+):
     """Основной метод загрузки данных из SQLite в Postgres"""
     # postgres_saver = PostgresSaver(pg_conn)
-    # sqlite_extractor = SQLiteExtractor(connection)
-
-    # data = sqlite_extractor.extract_movies()
-    # postgres_saver.save_all_data(data)
+    sqlite_extractor = SQLiteExtractor(sqlite_conn)
+    for db_dataclass in TABLES:
+        for chunk in sqlite_extractor.fetch_chunks(
+            db_dataclass.table_name, chunk_size
+        ):
+            obj_list = [db_dataclass(**item) for item in chunk]
+            print(obj_list)
+            print('\n\n')
 
 
 if __name__ == '__main__':
-    dsl = {'dbname': 'movies_database', 'user': 'app', 'password': '123qwe', 'host': '127.0.0.1', 'port': 5432}
-    with sqlite3.connect('db.sqlite') as sqlite_conn, psycopg2.connect(**dsl, cursor_factory=DictCursor) as pg_conn:
+    with sqlite_conn_context(SQLITE_DB_PATH) as sqlite_conn, psycopg2.connect(
+        **DSL, cursor_factory=DictCursor
+    ) as pg_conn:
         load_from_sqlite(sqlite_conn, pg_conn)
